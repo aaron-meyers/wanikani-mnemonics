00958af571dbbf26ca97dddf5c5c87634934429e
Aaron Meyers <13340718+aaron-meyers@users.noreply.github.com>
2023-06-01 11:26:47 -0700

    SubjectQueries.ps1 - list kanji with duplicate primary meanings in same level

---
+++ scripts/SubjectQueries.ps1
@@ -0,0 +1,4 @@
+$subjects = Get-Content '.\data\subjects.jsonl' | ConvertFrom-Json
+
+# List kanji with the same primary meaning in the same level
+$subjects|? object -eq 'kanji'|select id,@{n='characters';e={$_.data.characters}},@{n='meaning';e={($_.data.meanings|? primary -eq $true).meaning}},@{n='level';e={$_.data.level}}|group-object meaning,level|? count -gt 1
