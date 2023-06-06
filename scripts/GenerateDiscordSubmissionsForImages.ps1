e0cc9097fef055497f8405d95f74a07dea0a9646
Aaron Meyers <13340718+aaron-meyers@users.noreply.github.com>
2023-06-06 10:21:46 -0700

    GenerateDiscordSubmissionsForImages.ps1 complete

--- scripts/GenerateDiscordSubmissionsForImages.ps1
+++ scripts/GenerateDiscordSubmissionsForImages.ps1
@@ -4,7 +4,26 @@ Push-Location
 
 Set-Location '~\iCloudDrive\wanikani-mnemonics\pending-upload'
 
-#$subjects = & "$scriptsRoot\GetSubjects.ps1"
+$subjects = & "$scriptsRoot\GetSubjects.ps1"
+
+$subjectList = $subjects |
+    Select-Object id,object,@{n='url';e={$_.data.document_url}},@{n='characters';e={$_.data.characters}},@{n='meaning';e={($_.data.meanings|? primary -eq $true).meaning}},@{n='level';e={$_.data.level}}
+
+$typeMap = @{
+    'r' = 'radical'
+    'k' = 'kanji'
+    'v' = 'vocabulary'
+}
+$discordTypeMap = @{
+    'r' = 'Radical'
+    'k' = 'Kanji'
+    'v' = 'Vocab'
+}
+$mtypeMap = @{
+    'm' = 'Meaning'
+    'r' = 'Reading'
+    'mr' = 'Both'
+}
 
 Get-ChildItem *.jpg -Recurse |
     Foreach-Object {
@@ -19,9 +38,23 @@ Get-ChildItem *.jpg -Recurse |
         if ($dir -match "[0-9]+$") {
             $level = [int]($Matches[0])
         }
-        "$fileName, $key, $type, $mtype, $level"
+        #"$fileName, $key, $type, $mtype, $level"
 
-        #$subjects | Where-Object
+        $object = $typeMap[$type]
+        $discordType = $discordTypeMap[$type]
+        $mnemonictype = $mtypeMap[$mtype]
+
+        $subject = $subjectList |
+            Where-Object {$_.level -eq $level -and $_.meaning -eq $key -and $_.object -eq $object}
+        if ($subject.Count -eq 1) {
+            $char = ($type -eq 'r' ? $subject.meaning.ToLower() : $subject.characters)
+            Write-Output "========== $dir\$fileName =========="
+            Write-Output "/submit char:$char type:$discordType source:DALL-E 2 prompt:<don't remember> mnemonictype:$mnemonictype image:"
+            Write-Output $subject.url
+            Write-Output ''
+        } else {
+            Write-Warning "Multiple matches found for $key in level $level"
+        }
     }
 
 Pop-Location
